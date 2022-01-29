e37ec6584f1b3c954c3a90bdbac1a244c9220640
Somelina Obiechina <ifeasome@gmail.com>
2022-01-28 22:35:52 -0600

    API call test

--- PeopleFetcher.cs
+++ PeopleFetcher.cs
@@ -1,41 +1,56 @@
 using System;
 using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json.Linq;
 
 namespace CatWorx.BadgeMaker {
     class PeopleFetcher 
     {
         // Get employees method
-        public static List<Employee> GetEmployees()
-        {
-            List<Employee> employees = new List<Employee>();
-            while (true)
-            {
-                // Move the initial prompt inside the loop, so it repeats for each employee
-                Console.WriteLine("Enter first name (leave empty to exit): ");
+        // public static List<Employee> GetEmployees()
+        // {
+        //     List<Employee> employees = new List<Employee>();
+        //     while (true)
+        //     {
+        //         // Move the initial prompt inside the loop, so it repeats for each employee
+        //         Console.WriteLine("Enter first name (leave empty to exit): ");
 
-                // change input to firstName
-                string firstName = Console.ReadLine();
-                if (firstName == "")
-                {
-                    break;
-                }
+        //         // change input to firstName
+        //         string firstName = Console.ReadLine();
+        //         if (firstName == "")
+        //         {
+        //             break;
+        //         }
 
-                // add a Console.ReadLine() for each value
-                Console.Write("Enter last name: ");
-                string lastName = Console.ReadLine();
-                Console.Write("Enter ID: ");
-                int id = Int32.Parse(Console.ReadLine());
-                Console.Write("Enter Photo URL:");
-                string photoUrl = Console.ReadLine();
-                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
-                employees.Add(currentEmployee);
-            }
+        //         // add a Console.ReadLine() for each value
+        //         Console.Write("Enter last name: ");
+        //         string lastName = Console.ReadLine();
+        //         Console.Write("Enter ID: ");
+        //         int id = Int32.Parse(Console.ReadLine());
+        //         Console.Write("Enter Photo URL:");
+        //         string photoUrl = Console.ReadLine();
+        //         Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+        //         employees.Add(currentEmployee);
+        //     }
 
-            return employees;
-        }
+        //     return employees;
+        // }
+         // Get API method 
+        public static List<Employee> GetFromAPI() {
+           List<Employee> employees = new List<Employee>();
+           using (WebClient client = new WebClient())
+          {
+            // Image example
+            string response = client.DownloadString("https://randomuser.me/api/?results=10&nat=us&inc=name,id,picture");
+           JObject json = JObject.Parse(response);
+           Console.WriteLine(json);
+           }
+            
+           return employees;
+}
 
         
     }
 
-    // Get API method
+    
 }
--- Program.cs
+++ Program.cs
@@ -8,10 +8,10 @@ namespace CatWorx.BadgeMaker
 
         static void Main(string[] args)
         {
-             List<Employee> employees = PeopleFetcher.GetEmployees();
+            //  List<Employee> employees = PeopleFetcher.GetEmployees();
+             List<Employee> employees = PeopleFetcher.GetFromAPI();
             
-
-            // Util.PrintEmployees(employees);
+            Util.PrintEmployees(employees);
             Util.MakeCSV(employees);
             Util.MakeBadges(employees);
         }
